65de09378fca82f3bfb83d479c6edb4610e05cc8
metinars <arsmetin90@gmail.com>
2023-10-01 20:56:20 +0300

    Http and user detail form added

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -6,12 +6,18 @@ import MealItem from './MealsItem/MealItem';
 
 const AvaliableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         'https://react-http-3ff02-default-rtdb.firebaseio.com/meals.json'
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -25,9 +31,30 @@ const AvaliableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading..</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
